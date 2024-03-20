b9d1cbcc64df70e1862bf1990f53e19fca4f0d3f
Computer-Tsu <55188587+Computer-Tsu@users.noreply.github.com>
2024-03-20 15:53:38 -0400

    Update Enable Active Directory Recycle Bin.ps1

--- Enable Active Directory Recycle Bin.ps1
+++ Enable Active Directory Recycle Bin.ps1
@@ -1,5 +1,6 @@
 <#
   https://deployhappiness.com/please-enable-the-active-directory-recycle-bin/
+  To restore the object, you can use the Get-ADObject cmdlet with the -includeDeletedObjects parameter and pass that information to the Restore-ADObject cmdlet. The easier way is to just launch the Active Directory Administrative Center on a 2012R2/Windows 8.1+ machine. Navigate to the Deleted Objects container under your domain.
 #>
 
 Get-ADOptionalFeature -Filter *
